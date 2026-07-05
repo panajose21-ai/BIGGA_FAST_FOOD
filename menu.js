24e54451dde6f60fcf356fbca135568b7d2e6355
panajose21-ai <panajose21@gmail.com>
2026-07-04 21:06:46 -0500

    Crear menu.js

---
+++ menu.js
@@ -0,0 +1,12 @@
+module.exports = [
+  { id: "1", nombre: "Bigga Cheddar Fresa", precio: 20000 },
+  { id: "2", nombre: "Bigga Bacon Pretzel", precio: 23000 },
+  { id: "3", nombre: "Bigga Crispy Onion", precio: 26000 },
+  { id: "4", nombre: "Bigga Deluxe Pesto", precio: 28000 },
+  { id: "5", nombre: "Bigga Chorizo Búfalo", precio: 24000 },
+  { id: "6", nombre: "Bigga Piña Ahumada", precio: 25000 },
+  { id: "7", nombre: "Coca-Cola Mini 250ml", precio: 3500 },
+  { id: "8", nombre: "Coca-Cola Personal 400ml", precio: 4500 },
+  { id: "9", nombre: "Coca-Cola 1.5 Litros", precio: 9000 },
+  { id: "10", nombre: "Agua embotellada", precio: 4000 },
+];
